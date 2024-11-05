73d41622520a915d5556a7677703757ac6a8a519
Vishal <vishalpal1501@gmail.com>
2024-11-05 22:02:10 +0530

    primitive types

---
+++ Basic Types/basic.ts
@@ -0,0 +1,11 @@
+/*
+1. Primitives: number,string, boolean, undefined , null
+2. More Complex Type: array, objects, function type, parameter
+*/
+
+// Primitives
+let age: number = 20;
+
+let userName: string = 'Vishal Pal';
+
+let isInstructor: boolean = true;
