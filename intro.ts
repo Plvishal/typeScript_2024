03f1e82f91ebaf0f6f9f01091f0c713c26fcbc61
Vishal <vishalpal1501@gmail.com>
2024-11-05 21:55:49 +0530

    How to run type script file

---
+++ intro.ts
@@ -0,0 +1,17 @@
+function add(a: number, b: number) {
+  return a + b;
+}
+
+console.log(add(2, 3));
+
+
+
+
+/*
+How to run type script file
+1. create file with extention of ts like(intro.ts)
+2. After that open terminal and type tsx intro.ts
+3. when you above command then typeScript generates a javaScript file like (intro.js)
+4. Then run this file or link to your project (node intro.js)
+
+*/
