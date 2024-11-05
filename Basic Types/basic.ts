509a5fa468e07642e9ff0731771a8456027139eb
Vishal <vishalpal1501@gmail.com>
2024-11-05 22:10:45 +0530

    Array and Objects types

--- Basic Types/basic.ts
+++ Basic Types/basic.ts
@@ -9,3 +9,28 @@ let age: number = 20;
 let userName: string = 'Vishal Pal';
 
 let isInstructor: boolean = true;
+
+//  More Complex Type
+let hobbies: string[]; // Array type
+hobbies = ['sport', 'cooking'];
+
+//object type
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: 'Vishal Pal',
+  age: 24,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
+// Define Array of objects types
+people = [
+  { name: 'Raju', age: 25 },
+  { name: 'Shyam', age: 23 },
+];
